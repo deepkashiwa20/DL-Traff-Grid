43ac6cef04782692c7068d8e712d2918c4c92957
deepkashiwa20 <54202779+deepkashiwa20@users.noreply.github.com>
2021-06-11 20:49:42 +0900

    Delete workBousai_tokyo directory

--- workBousai_tokyo/predflowio/Param_STResNet.py
+++
@@ -1,5 +0,0 @@
-nb_res_units = 2
-nb_channel = 2     # inflow/outflow
-len_c, len_p, len_t = 3, 1, 1
-dayInfo = True
-DAYTIMESTEP = 24 * 2    # half an hour time interval
--- workBousai_tokyo/predflowio/line/LINE-master/linux/evaluate/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd liblinear
-make
-cd ..
-
-cd program
-g++ -O2 preprocess.cpp -o preprocess
-g++ -O2 score.cpp -o score
